60d2059307d6696388bd02fbf04e60420f73211d
Noah Campbell <ntcampbell03@gmail.com>
2022-08-01 17:57:29 -0500

    improved code

--- wordle2.py
+++ wordle2.py
@@ -1,45 +1,48 @@
-fp = open("wordle_words", "r")
 import random
-words = " ".join(fp.readlines())
-words = words.split("\n")
-all_words = []
-for word in words:
-    word = word.strip()
-    all_words.append(word)
-for i in range(random.randint(0, 9111)):
-    word = all_words[i]
+
+fp = open("wordle_words", "r") # Creates wordbank
+wordBank = "".join(fp.readlines())
+wordBank = wordBank.split("\n")
 fp.close()
-count = 0
-def wordle(guess):
-    if count == 6:
-        return "you lose suck it"
-    if guess == "exit":
-        print("penis, the word was", word)
-    elif guess not in all_words:
-        print("Not in dictionary")
-        guess = input("Input word: ")
-        return wordle(guess)
-    elif guess != word:
-        new_guess = ""
-        for i, char in enumerate(guess):
-            if char == word[i]:
-                new_guess += "\033[0;30;42m" + guess[i] + "\033[0;30;47m"
-            elif char in word:
-                new_guess += "\033[0;30;43m" + guess[i] + "\033[0;30;47m"
+
+def chooseWord(): # Chooses random word from wordbank
+    for i in range(random.randint(0, 9111)):
+        targetWord = wordBank[i]
+    return targetWord
+
+def guess(guessWord, targetWord): # Determines correct letters in guess
+    if guessWord == "exit":
+        print("Exited, the word was", targetWord)
+        return True
+    elif guessWord != targetWord:
+        correct = ""
+        for i, char in enumerate(guessWord):
+            if char == targetWord[i]:
+                correct += "\033[0;30;42m" + guessWord[i] + "\033[0;30;47m"
+            elif char in targetWord:
+                correct += "\033[0;30;43m" + guessWord[i] + "\033[0;30;47m"
             else:
-                new_guess += "\033[0;30;47m" + guess[i]
-        print(new_guess)
-        count += 1
-        guess = input("\033[0;30;47m Input word: ")
-        return wordle(guess)
-    elif guess == word:
-        print("\033[0;30;42m", guess, "\033[0;30;47m")
+                correct += "\033[0;30;47m" + guessWord[i]
+        print(correct)
+    elif guessWord == targetWord:
+        print("\033[0;30;42m", guessWord, "\033[0;30;47m")
         print("\033[0;30;42m you won good job now piss off", "\033[0;30;47m")
-        play = input("Play again? y/n: ")
-        if play == "y" or play == "yes":
-            guess = input("Input word: ")
-            for i in range(random.randint(0, 9111)):
-                word = all_words[i]
-                count = 0
-        else:
-            return "penis"
+        return True
+
+def wordle(): # Plays wordle
+    targetWord = chooseWord()
+    guessCount = 0
+    done = False
+    while not done:
+        guessWord = input("Enter guess: ")
+        if guessWord not in wordBank:
+            print("Not in dictionary")
+            continue
+        done = guess(guessWord, targetWord)
+        if guessCount == 6:
+            print("L, word was", targetWord)
+            done = True
+        guessCount += 1
+    playAgain = input("Do you want to play again? (Y/n): ")
+    if playAgain == "Y" or playAgain == "y":
+        wordle()
