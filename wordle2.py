c2a55c626d7ee6c9e856c032c957c8c27b78bd49
Noah Campbell <ntcampbell03@gmail.com>
2022-08-02 09:25:18 -0500

    added Wordle Class

--- wordle2.py
+++ wordle2.py
@@ -5,44 +5,51 @@ wordBank = "".join(fp.readlines())
 wordBank = wordBank.split("\n")
 fp.close()
 
-def chooseWord(): # Chooses random word from wordbank
-    for i in range(random.randint(0, 9111)):
-        targetWord = wordBank[i]
-    return targetWord
+class Wordle:
+    def __init__(self, wordBank):
+        self.wordBank = wordBank
 
-def guess(guessWord, targetWord): # Determines correct letters in guess
-    if guessWord == "exit":
-        print("Exited, the word was", targetWord)
-        return True
-    elif guessWord != targetWord:
-        correct = ""
-        for i, char in enumerate(guessWord):
-            if char == targetWord[i]:
-                correct += "\033[0;30;42m" + guessWord[i] + "\033[0;30;47m"
-            elif char in targetWord:
-                correct += "\033[0;30;43m" + guessWord[i] + "\033[0;30;47m"
+    def chooseWord(self): # Chooses random word from wordbank
+        for i in range(random.randint(0, 9111)):
+            targetWord = self.wordBank[i]
+        return targetWord
+
+    def guess(self, guessWord, targetWord): # Determines correct letters in guess
+        if guessWord != targetWord:
+            correct = ""
+            for i, char in enumerate(guessWord):
+                if char == targetWord[i]:
+                    correct += "\033[0;30;42m" + guessWord[i] + "\033[0;30;47m"
+                elif char in targetWord:
+                    correct += "\033[0;30;43m" + guessWord[i] + "\033[0;30;47m"
+                else:
+                    correct += "\033[0;30;47m" + guessWord[i]
+            print(correct)
+        elif guessWord == targetWord:
+            print("\033[0;30;42m", guessWord, "\033[0;30;47m")
+            print("\033[0;30;42m you won good job now piss off", "\033[0;30;47m")
+            return True
+
+    def play(self): # Plays wordle
+        targetWord = self.chooseWord()
+        guessCount = 0
+        done = False
+        while not done:
+            guessWord = input("Enter guess: ")
+            if guessWord == "exit":
+                print("Exited, the word was", targetWord)
+                break
+            elif guessWord not in self.wordBank:
+                print("Not in dictionary")
+                continue
             else:
-                correct += "\033[0;30;47m" + guessWord[i]
-        print(correct)
-    elif guessWord == targetWord:
-        print("\033[0;30;42m", guessWord, "\033[0;30;47m")
-        print("\033[0;30;42m you won good job now piss off", "\033[0;30;47m")
-        return True
-
-def wordle(): # Plays wordle
-    targetWord = chooseWord()
-    guessCount = 0
-    done = False
-    while not done:
-        guessWord = input("Enter guess: ")
-        if guessWord not in wordBank:
-            print("Not in dictionary")
-            continue
-        done = guess(guessWord, targetWord)
-        if guessCount == 6:
-            print("L, word was", targetWord)
-            done = True
-        guessCount += 1
-    playAgain = input("Do you want to play again? (Y/n): ")
-    if playAgain == "Y" or playAgain == "y":
-        wordle()
+                done = self.guess(guessWord, targetWord)
+                if guessCount == 6:
+                    print("L, word was", targetWord)
+                    done = True
+                guessCount += 1
+        playAgain = input("Do you want to play again? (Y/n): ")
+        if playAgain == "Y" or playAgain == "y":
+            self.play()
+            
+Wordle = Wordle(wordBank)
